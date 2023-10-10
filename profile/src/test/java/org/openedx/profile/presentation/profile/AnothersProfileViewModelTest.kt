0587e9101b965166fa0dfaaabca3acbb9eb331f1
PavloNetrebchuk <pavlo.netrebchuk@raccoongang.com>
2023-10-10 16:26:21 +0300

    Added AnothersProfileViewModel unit tests

---
+++ profile/src/test/java/org/openedx/profile/presentation/profile/AnothersProfileViewModelTest.kt
@@ -0,0 +1,121 @@
+package org.openedx.profile.presentation.profile
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.openedx.core.R
+import org.openedx.core.UIMessage
+import org.openedx.core.domain.model.ProfileImage
+import org.openedx.core.system.ResourceManager
+import org.openedx.profile.domain.interactor.AnothersProfileInteractor
+import org.openedx.profile.presentation.anothers_account.AnothersProfileUIState
+import org.openedx.profile.presentation.anothers_account.AnothersProfileViewModel
+import java.net.UnknownHostException
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class AnothersProfileViewModelTest {
+
+    @get:Rule
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    private val dispatcher = StandardTestDispatcher()
+
+    private val resourceManager = mockk<ResourceManager>()
+    private val interactor = mockk<AnothersProfileInteractor>()
+    private val username = "username"
+
+    private val account = org.openedx.profile.domain.model.Account(
+        username = "",
+        bio = "",
+        requiresParentalConsent = false,
+        name = "",
+        country = "",
+        isActive = true,
+        profileImage = ProfileImage("", "", "", "", false),
+        yearOfBirth = 2000,
+        levelOfEducation = "",
+        goals = "",
+        languageProficiencies = emptyList(),
+        gender = "",
+        mailingAddress = "",
+        email = "",
+        dateJoined = null,
+        accountPrivacy = org.openedx.profile.domain.model.Account.Privacy.PRIVATE
+    )
+
+    private val noInternet = "Slow or no internet connection"
+    private val somethingWrong = "Something went wrong"
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(dispatcher)
+        every { resourceManager.getString(R.string.core_error_no_connection) } returns noInternet
+        every { resourceManager.getString(R.string.core_error_unknown_error) } returns somethingWrong
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `getAccount no internetConnection`() = runTest {
+        val viewModel = AnothersProfileViewModel(
+            interactor,
+            resourceManager,
+            username
+        )
+        coEvery { interactor.getAccount(username) } throws UnknownHostException()
+        advanceUntilIdle()
+
+        coVerify(exactly = 1) { interactor.getAccount(username) }
+
+        val message = viewModel.uiMessage.value as? UIMessage.SnackBarMessage
+        assert(viewModel.uiState.value is AnothersProfileUIState.Loading)
+        assertEquals(noInternet, message?.message)
+    }
+
+    @Test
+    fun `getAccount unknown exception`() = runTest {
+        val viewModel = AnothersProfileViewModel(
+            interactor,
+            resourceManager,
+            username
+        )
+        coEvery { interactor.getAccount(username) } throws Exception()
+        advanceUntilIdle()
+
+        coVerify(exactly = 1) { interactor.getAccount(username) }
+
+        val message = viewModel.uiMessage.value as? UIMessage.SnackBarMessage
+        assert(viewModel.uiState.value is AnothersProfileUIState.Loading)
+        assertEquals(somethingWrong, message?.message)
+    }
+
+    @Test
+    fun `getAccount success`() = runTest {
+        val viewModel = AnothersProfileViewModel(
+            interactor,
+            resourceManager,
+            username
+        )
+        coEvery { interactor.getAccount(username) } returns account
+        advanceUntilIdle()
+
+        coVerify(exactly = 1) { interactor.getAccount(username) }
+
+        assert(viewModel.uiState.value is AnothersProfileUIState.Data)
+        assert(viewModel.uiMessage.value == null)
+    }
+}
