1cd17bcd14bf05bdc1b469c2857a4d0e5ff9c00e
Farhan Arshad <farshad@2u.com>
2023-11-22 13:53:18 +0500

    build: refactor build script for built src

---
+++ buildSrc/src/main/groovy/org/edx/builder/FirebaseConfig.groovy
@@ -0,0 +1,8 @@
+package org.edx.builder
+
+class FirebaseConfig {
+    public String projectId = ""
+    public String appId = ""
+    public String apiKey = ""
+    public String gcmSenderId = ""
+}
