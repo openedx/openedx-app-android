0b709c4e03c4fb7e3e6719a74576a14428ce4975
Farhan Arshad <farshad@2u.com>
2024-01-19 16:35:14 +0500

    fix: address PR comments

--- core/src/main/java/org/openedx/core/Validator.kt
+++ core/src/main/java/org/openedx/core/Validator.kt
@@ -4,14 +4,14 @@ import java.util.regex.Pattern
 
 class Validator {
 
-    fun isEmailOrUserNameValid(email: String): Boolean {
-        return if (email.contains("@")) {
+    fun isEmailOrUserNameValid(input: String): Boolean {
+        return if (input.contains("@")) {
             val validEmailAddressRegex = Pattern.compile(
                 "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE
             )
-            validEmailAddressRegex.matcher(email).find()
+            validEmailAddressRegex.matcher(input).find()
         } else {
-            email.isBlank().not()
+            input.isNotBlank() && input.contains(" ").not()
         }
     }
 
