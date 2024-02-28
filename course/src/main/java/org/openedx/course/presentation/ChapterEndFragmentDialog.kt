985022b0d1c9a37a82ceaef5d7f24668161ad9af
droid <dixidroid@gmail.com>
2024-02-28 18:50:22 +0100

    fix: bug when unable to go back to outline

--- course/src/main/java/org/openedx/course/presentation/ChapterEndFragmentDialog.kt
+++ course/src/main/java/org/openedx/course/presentation/ChapterEndFragmentDialog.kt
@@ -8,8 +8,21 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
@@ -72,10 +85,7 @@ class ChapterEndFragmentDialog : DialogFragment() {
                         onBackButtonClick = {
                             dismiss()
                             listener?.onDismiss()
-                            requireActivity().supportFragmentManager.popBackStack(
-                                CourseSectionFragment::class.java.simpleName,
-                                0
-                            )
+                            requireActivity().supportFragmentManager.popBackStack()
                         },
                         onProceedButtonClick = {
                             dismiss()
