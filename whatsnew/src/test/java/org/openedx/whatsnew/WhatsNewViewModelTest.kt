89a1e834028da6f41bc124fadaf25234076f915e
omerhabib26 <omer.habib26@gmail.com>
2024-03-27 20:08:58 +0500

    fix: test cases

--- whatsnew/src/test/java/org/openedx/whatsnew/WhatsNewViewModelTest.kt
+++ whatsnew/src/test/java/org/openedx/whatsnew/WhatsNewViewModelTest.kt
@@ -5,6 +5,8 @@ import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
+import org.openedx.core.presentation.global.AppData
+import org.openedx.whatsnew.data.storage.WhatsNewPreferences
 import org.openedx.whatsnew.domain.model.WhatsNewItem
 import org.openedx.whatsnew.presentation.WhatsNewAnalytics
 import org.openedx.whatsnew.presentation.whatsnew.WhatsNewViewModel
@@ -13,6 +15,9 @@ class WhatsNewViewModelTest {
 
     private val whatsNewManager = mockk<WhatsNewManager>()
     private val analytics = mockk<WhatsNewAnalytics>()
+    private val router = mockk<WhatsNewRouter>()
+    private val preferencesManager = mockk<WhatsNewPreferences>()
+    private val appData = mockk<AppData>()
 
     private val whatsNewItem = WhatsNewItem(
         version = "1.0.0",
@@ -23,7 +28,15 @@ class WhatsNewViewModelTest {
     fun `getNewestData success`() = runTest {
         every { whatsNewManager.getNewestData() } returns whatsNewItem
 
-        val viewModel = WhatsNewViewModel("", "", whatsNewManager, analytics)
+        val viewModel = WhatsNewViewModel(
+            "",
+            "",
+            whatsNewManager,
+            analytics,
+            router,
+            preferencesManager,
+            appData
+        )
 
         verify(exactly = 1) { whatsNewManager.getNewestData() }
         assert(viewModel.whatsNewItem.value == whatsNewItem)
