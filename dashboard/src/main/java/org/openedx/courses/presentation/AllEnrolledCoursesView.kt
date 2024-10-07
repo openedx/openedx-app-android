eddd54b357e6d0da4ccd1dc2d3df5832212600f7
Igor <dixidroid@users.noreply.github.com>
2024-10-07 11:14:39 +0200

    fix: bug when unable to see new loaded items on the All Courses screen (#382)

--- dashboard/src/main/java/org/openedx/courses/presentation/AllEnrolledCoursesView.kt
+++ dashboard/src/main/java/org/openedx/courses/presentation/AllEnrolledCoursesView.kt
@@ -20,6 +20,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
@@ -329,7 +330,7 @@ private fun AllEnrolledCoursesView(
                                                             }
                                                         )
                                                     }
-                                                    item {
+                                                    item(span = { GridItemSpan(columns) }) {
                                                         if (state.canLoadMore) {
                                                             Box(
                                                                 modifier = Modifier
