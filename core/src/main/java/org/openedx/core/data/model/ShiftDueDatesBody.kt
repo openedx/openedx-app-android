a448954e8e1735d98e3dd141e15a302a2bc8910a
PavloNetrebchuk <pavlo.netrebchuk@raccoongang.com>
2025-03-27 18:12:09 +0200

    fix: pagination bugs

--- core/src/main/java/org/openedx/core/data/model/ShiftDueDatesBody.kt
+++
@@ -1,7 +0,0 @@
-package org.openedx.core.data.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ShiftDueDatesBody(
-    @SerializedName("course_keys") val courseKeys: List<String>
-)
